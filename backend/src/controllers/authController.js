84d0bda6aa6e638b240dfcdddaf9113caa123b63
saigoud17 <ksai36499@gmail.com>
2025-11-08 23:15:52 +0530

    Replaced bcrypt with bcryptjs for Render deployment

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { User } = require('../models');
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,5 +1,5 @@
 const { DataTypes, Model } = require('sequelize');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 class User extends Model {
     static initModel(sequelize) {
